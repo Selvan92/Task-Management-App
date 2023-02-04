36922832a23447649996e01c266d5c5c241629f6
Satya Selvan <satyaat92@gmail.com>
2023-02-05 07:59:17 +0800

    routes and js files added ready to test in insomania

---
+++ backend/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+        'no-console': 0,
+        'import/extensions': 0,
+        'no-underscore-dangle': 0,
+    }
+}
