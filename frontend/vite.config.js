c3c4397d2abaf097eae57a83474e00452d95ae2b
Satya Selvan <satyaat92@gmail.com>
2023-02-06 18:43:11 +0800

    deploying

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        // target: 'http://localhost:8000',
+        target: 'https://git.heroku.com/eerie-skeleton-50764.git'
       },
     },
   },
