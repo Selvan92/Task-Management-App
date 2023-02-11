c73c16f2e9a5fe5ed657e6e774ff988d6ad10fae
Satya Selvan <satyaat92@gmail.com>
2023-02-11 17:53:51 +0800

    heroku try

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,14 +5,14 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   proxy: {
-  //     '/api': {
-  //        target: 'http://localhost:8000',
+  server: {
+    proxy: {
+      '/api': {
+         target: 'https://eerie-skeleton-50764.herokuapp.com/',
      
-  //    },
-  //  },
-  // },
+     },
+   },
+  },
  
     // build: {
     //   // generate manifest.json in outDir
