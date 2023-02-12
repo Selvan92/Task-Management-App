f6115b248a2db41667a2df2307fe8d9c84eb951e
Satya Selvan <satyaat92@gmail.com>
2023-02-12 11:40:40 +0800

    added target again

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,16 +5,15 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   proxy: {
-  //     '/api': {
-  //       // target: 'http://localhost:8000',
-
-  //         target: 'https://task-management-backend-nkpf.onrender.com',
+  server: {
+    proxy: {
+      '/api': {
+       
+          target: 'https://task-management-backend-nkpf.onrender.com',
      
-  //    },
-  //  },
-  // },
+     },
+   },
+  },
  
     // build: {
     //   // generate manifest.json in outDir
