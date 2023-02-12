43001f335d96f26846f49f825558cd31e05e6b18
Satya Selvan <satyaat92@gmail.com>
2023-02-12 13:55:46 +0800

    http set to false

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -40,7 +40,8 @@ export const login = async (req, res, next) => {
     });
     return res
       .cookie('access_token', token, {
-        httpsOnly: true,
+        httpOnly: false,
+        secure:true
       })
       .status(200)
       .json({ name: user.name, email: user.email, message: 'login success' });
