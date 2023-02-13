55c8f1ddcd836a9771673d250ac9ae4f718889fd
Satya Selvan <satyaat92@gmail.com>
2023-02-13 18:46:05 +0800

    try removing the added credential code in server

--- backend/index.js
+++ backend/index.js
@@ -15,12 +15,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", req.headers.origin);
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+
 
 
 // middleware
