f63a8ac7272c270905ac5aecf53da2e04e8262cb
Satya Selvan <satyaat92@gmail.com>
2023-02-13 21:16:31 +0800

    self try debugd

--- backend/index.js
+++ backend/index.js
@@ -27,19 +27,19 @@ app.use(cookieParser());
 // routes
 
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://task-management-frontend-cg1s.onrender.com");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 app.options("*", function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "https://task-management-frontend-cg1s.onrender.com");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Credentials", "false");
   res.send();
+});
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://task-management-frontend-cg1s.onrender.com");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
 app.use('/api', allRoutes);
