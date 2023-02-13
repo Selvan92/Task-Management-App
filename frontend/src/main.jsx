f8785e574f6365fb5bac6d5be321d98d76bae8a4
Satya Selvan <satyaat92@gmail.com>
2023-02-13 18:33:18 +0800

    revert the main.jsx axios defaults

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,7 @@ import App from './App';
 import './styles/global.scss'; // comment this line while running local
 import getApiBaseUrl from './utils/getApiBaseUrl'; // comment this line while running loca
 
-axios.defaults.withCredentials = true;
+// axios.defaults.withCredentials = true;
 axios.defaults.baseURL = getApiBaseUrl(); // comment this line while running local
 
 ReactDOM.createRoot(document.getElementById('root')).render(
