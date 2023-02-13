c32ea8b99f9b809e9f12c22586c26ce9e74869cc
Satya Selvan <satyaat92@gmail.com>
2023-02-13 20:28:35 +0800

    added options

--- backend/index.js
+++ backend/index.js
@@ -35,6 +35,13 @@ app.use(function(req, res, next) {
   next();
 });
 
+app.options("*", function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://task-management-frontend-cg1s.onrender.com");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.status(200).end();
+});
 
 app.use('/api', allRoutes);
 
