897c06c0924c9f298bc6243eb4c9344b844fbadf
Satya Selvan <satyaat92@gmail.com>
2023-02-13 18:53:07 +0800

    changed to false on app.use in index.js server

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.use(cookieParser());
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", req.headers.origin);
-  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Credentials", "false");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
