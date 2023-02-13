dfc5f74afe1ea94d65dc972ae21e4149efda37da
Satya Selvan <satyaat92@gmail.com>
2023-02-13 20:08:41 +0800

    removed options

--- backend/index.js
+++ backend/index.js
@@ -30,16 +30,9 @@ app.use(cookieParser());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "https://task-management-frontend-cg1s.onrender.com");
   res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
-});
-app.options("*", function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://task-management-frontend-cg1s.onrender.com");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.send();
 });
 
 
