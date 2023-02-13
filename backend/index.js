c5e3795dd004160c0a722570ad236052fccd680b
Satya Selvan <satyaat92@gmail.com>
2023-02-13 20:39:11 +0800

    added cors

--- backend/index.js
+++ backend/index.js
@@ -27,21 +27,10 @@ app.use(cookieParser());
 // routes
 
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://task-management-frontend-cg1s.onrender.com");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-app.options("*", function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://task-management-frontend-cg1s.onrender.com");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.status(200).end();
-});
+app.use(cors());
+app.use(cors({
+  origin: 'https://task-management-frontend-cg1s.onrender.com'
+}));
 
 app.use('/api', allRoutes);
 
