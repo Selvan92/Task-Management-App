025cd1fb14fb01aaade53c472f696819734e5c80
Satya Selvan <satyaat92@gmail.com>
2023-02-13 19:54:07 +0800

    debug access contl

--- backend/index.js
+++ backend/index.js
@@ -26,13 +26,13 @@ app.use(cookieParser());
 
 // routes
 
-app.options("*", function(req, res, next) {
+app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "https://task-management-frontend-cg1s.onrender.com");
+  res.header("Access-Control-Allow-Credentials", "false");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Credentials", "false");
-  res.send();
-}); (e
+  next();
+});
 
 app.use('/api', allRoutes);
 
