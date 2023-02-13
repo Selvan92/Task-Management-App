ee6ed5534cfb4a4ab996755cc8163a9cb1fc80ff
Satya Selvan <satyaat92@gmail.com>
2023-02-13 18:51:21 +0800

    added the code again

--- backend/index.js
+++ backend/index.js
@@ -25,6 +25,14 @@ app.use(express.json());
 app.use(cookieParser());
 
 // routes
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/api', allRoutes);
 
 // error handler
